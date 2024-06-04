41c25bc898aa444c8b1e9a4cf6de6faba678cb58
fany <fany_lin@worldvision.org.tw>
2024-06-04 10:23:54 +0800

    feat: element-plus setup (on demand import)

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,8 +2,14 @@
   <div class="about">
     <h1>This is an about page</h1>
     <p class="text-red-500">And me, hello</p>
+    <el-switch v-model="value1" />
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+const value1 = ref(true)
+</script>
 
 <style>
 @media (min-width: 1024px) {
